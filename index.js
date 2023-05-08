d279a0dd379dd990eb6eac017d905b8f5becf216
Mathew <kemp.business@outlook.com>
2023-05-08 16:48:42 +1000

    added sprite sheets for player cropped and positions player rendered and added not for ref later

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+//creates canvas as constant and gives context (2d or 3d)
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+//creates canvas size
 canvas.width = 1024
 canvas.height = 576
 
+//gives canvas a fill colour and internal size for fill
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+//creates containers for map and player
 const image = new Image()
 image.src = './img/gameMap.png'
-console.log(image)
 
+const playerImg = new Image()
+playerImg.src = './img/char/playerDown.png'
+
+//loads map and player in order, crops play sprite sheet
 image.onload = () => {
-    c.drawImage (image, 0, 0)
+    c.drawImage (image, 180, -70)
+     c.drawImage (playerImg, 
+        
+        0,//crop position for Spritesheet
+        0,
+        playerImg.width / 4,//crop size
+        playerImg.height,
+        canvas.width / 2 - playerImg.width / 4 / 2, //render location using full size of sprite sheet
+        canvas.height / 2 - playerImg.height / 2,
+        playerImg.width / 4,
+        playerImg.height)
 }
