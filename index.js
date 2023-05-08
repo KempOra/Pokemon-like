f7450f7ad5d93a7b3748e6a6e71fba78831574c0
Mathew <kemp.business@outlook.com>
2023-05-08 16:23:02 +1000

    revamped map zoom level added canvas call to map corrected map positioning

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const canvas = document.querySelector('canvas')
-const cxt = canvas.getContext('2d')
+const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
 
-cxt.fillStyle = 'white'
-cxt.fillRect(0, 0, canvas.width, canvas.height)
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img/gameMap.png'
+console.log(image)
+
+image.onload = () => {
+    c.drawImage (image, 0, 0)
+}
